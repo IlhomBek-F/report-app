48f6bc9a56b38f81ab34f57ea49fa4eed555c1b1
Ilhomjon <futurewithme1221@gmail.com>
2024-09-02 14:56:21 +0500

    refac: Refactored useQuery

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -11,52 +11,52 @@ interface ActionsModel {
   setRates: (rates: ReportDataModel[]) => void;
 }
 
-function Actions({chart, data, setRates}: ActionsModel) {
- const resData = useRef<null | ReportDataModel[]>([]);
+function Actions({ chart, data, setRates }: ActionsModel) {
+  const resData = useRef<null | ReportDataModel[]>([]);
 
- const calcMutation = useMutation({
-        mutationFn: (payload: ReportDataModel[]) => calculateCoef(payload),
-        onSuccess(data) {
-          resData.current = data;
-          setRates(data.slice(data.length - 4))
-        },
-        onError(error) {
-          alert(error.message)
-        },
- });
+  const calcMutation = useMutation({
+    mutationFn: (payload: ReportDataModel[]) => calculateCoef(payload),
+    onSuccess(data) {
+      resData.current = data;
+      setRates(data.slice(data.length - 4))
+    },
+    onError(error) {
+      alert(error.message)
+    },
+  });
 
   const handleYearChange = (year: number) => {
-    chart.current.data.labels = Array.from({length: 8}).map(() => year);
+    chart.current.data.labels = Array.from({ length: 8 }).map(() => year);
     chart.current.update();
   }
 
   const handleCalculationKoef = () => {
-    const payloadData = data.map((d: ReportDataModel) => ({...d, year: chart.current?.data.labels?.[0]}));
+    const payloadData = data.map((d: ReportDataModel) => ({ ...d, year: chart.current?.data.labels?.[0] }));
     calcMutation.mutate(payloadData)
   }
 
   const handleExportCsv = () => {
-    if(resData.current?.length) {
-        downloadCSV(resData.current as ReportDataModel[])
+    if (resData.current?.length) {
+      downloadCSV(resData.current as ReportDataModel[])
     }
   }
 
-    return (
-       <div className="actions">
-        <Button type="primary" onClick={handleCalculationKoef}> calculate koef</Button>
-        <Button type="primary" onClick={handleExportCsv}>Export</Button>
-        <Select
+  return (
+    <div className="actions">
+      <Button type="primary" onClick={handleCalculationKoef}> calculate koef</Button>
+      <Button type="primary" onClick={handleExportCsv}>Export</Button>
+      <Select
         defaultValue={2030}
         style={{ width: 80 }}
         onChange={handleYearChange}
         options={[
-        { value: 2030, label: 2030 },
-        { value: 2040, label: 2040 },
-        { value: 2050, label: 2050 },
-       ]}
-    />
-     </div>
-    )
+          { value: 2030, label: 2030 },
+          { value: 2040, label: 2040 },
+          { value: 2050, label: 2050 },
+        ]}
+      />
+    </div>
+  )
 }
 
-export {Actions}
+export { Actions }
--- src/service/index.ts
+++ src/service/index.ts
@@ -2,28 +2,40 @@ import { ReportDataModel } from "../core";
 
 const BASE_API = 'http://147.45.184.63:5000';
 
-async function getData() {
-    const response = await fetch(BASE_API);
+async function defaultQueryFn({ queryKey }: { queryKey: string[] }) {
+  const res = await fetch(`${BASE_API}/${queryKey[0]}`);
 
-    if (!response.ok) {
-      throw new Error(`Error fetching data: ${response.status} ${response.statusText}`);
-    }
-    
-    return response.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch')
+  }
+
+  return res.json();
+}
+
+async function getData() {
+  const response = await fetch(BASE_API);
+
+  if (!response.ok) {
+    throw new Error(`Error fetching data: ${response.status} ${response.statusText}`);
+  }
+
+  return response.json();
 }
 
 async function calculateCoef(data: ReportDataModel[]) {
- const response = await fetch(`${BASE_API}/calc`, 
-  {method: 'POST', body: JSON.stringify(data), headers: {
-    "Content-Type": "application/json",
-  }}
-);
+  const response = await fetch(`${BASE_API}/calc`,
+    {
+      method: 'POST', body: JSON.stringify(data), headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  );
 
- if(!response.ok) {
-  throw new Error(`Error calculating data: ${response.status} ${response.statusText}`)
- }
+  if (!response.ok) {
+    throw new Error(`Error calculating data: ${response.status} ${response.statusText}`)
+  }
 
- return response.json()
+  return response.json()
 }
 
-export {getData, calculateCoef}
+export { getData, calculateCoef, defaultQueryFn }
